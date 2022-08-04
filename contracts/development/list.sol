ef2d95716f52451101940d35a035853638ec8738
Alejo Lovallo <alejoe.lovallo@gmail.com>
2022-08-04 18:28:18 -0300

    feat(list): done

--- contracts/development/list.sol
+++ contracts/development/list.sol
@@ -2,39 +2,62 @@
 pragma solidity ^0.8.9;
 
 contract List {
-    uint256 [] public list;
-    mapping(uint256 => uint256) idList;
+    struct UserData {
+        address userAddr;
+        uint256 score;
+    }
+
+    UserData [] public userDataList;
+    mapping(address => uint256) userInfoList;
 
     event Initialized();
-    event Add(uint256 number, address indexed who);
-    event Remove(uint256 number, address indexed who);
+    event Add(address user,uint256 score, address who);
+    event Remove(address user, address who);
 
-    constructor(uint256[] memory values) {
-        uint256 length = values.length;
+    constructor(address[] memory _userAddresses, uint256[] memory _scores) {
+        uint256 length = _userAddresses.length;
         for(uint256 i = 0; i < length; i++){
-            list.push(values[i])
-            idList[values[i]] = list.length
+            /**
+            Add values
+             */
+            userDataList.push(
+                UserData({
+                    userAddr: _userAddresses[i],
+                    score: _scores[i]
+                })
+            );
+            /**
+            * Set that the user info is located in that position
+             */
+            userInfoList[_userAddresses[i]] = userDataList.length;
         }
+        emit Initialized();
     }
 
     function listLength() external view returns(uint256){
-        return list.length;
+        return userDataList.length;
     }
 
-    function add(uint256 value) public {
-        list.push(value)
-        idList[value] = list.length
+    function add(address _userAddress, uint256 _score) public {
+        userDataList.push(
+            UserData({
+                userAddr: _userAddress,
+                score: _score
+            })
+        );
+        userInfoList[_userAddress] = userDataList.length;
 
-        emit Add(value,msg.sender)
+        emit Add(_userAddress,_score,msg.sender);
     }
 
-    function remove(uint256 position) public {
-        uint256 index = idList[position] - 1;
-
-        list[index] = farmInfoList[farmInfoList.length - 1];
-        farmInfoList.pop();
-        farmIdList[_farmingAddr] = 0;
-        emit RemoveFarm(_farmingAddr);
+    function remove(address _userAddress) public {
+        uint256 index = userInfoList[_userAddress] - 1;
+        /*Make the replacement */
+        userDataList[index] = userDataList[userDataList.length - 1];
+        userDataList.pop();
+        /*MARKS THE ADDRESS WITH A ZERO INDICATING IT HAS NO SCORE*/
+        userInfoList[_userAddress] = 0;
+        emit Remove(_userAddress, msg.sender);
     }
 
 }
